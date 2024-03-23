a59083da9e94077e51332d151e349d1fd264ccc5
abdou fk <abdourrhamanefarkhsi@gmail.com>
2024-03-23 18:22:04 +0100

    deploy with links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import SocialMedias from './components/SocialMedias/SocialMedias';
 import HomePage from './pages/home';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Router>
-        <NavBar />
-        <SocialMedias />
-        <Routes>
-          <Route path="/portfolio/" element={<HomePage />} />
-          <Route path="/portfolio/mesprojets" element={<HomePage />} />
-          <Route path="/portfolio/contacts" element={<HomePage />} />
-        </Routes>
-      </Router>
+      {/* <Router> */}
+      <NavBar />
+      <SocialMedias />
+      <Routes>
+        <Route path="/portfolio/" element={<HomePage />} />
+        <Route path="/portfolio/mesprojets" element={<HomePage />} />
+        <Route path="/portfolio/contacts" element={<HomePage />} />
+      </Routes>
+      {/* </Router> */}
     </>
   );
 }
